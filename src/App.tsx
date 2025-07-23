5c3c74a84f3ab69dc28fb41b2ea880b2420f9865
Jiho Bok <bok.jiho@gmail.com>
2025-07-23 12:15:41 +0100

    Feat: Add emotion based recommendation form

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,101 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
+type Form = {
+	question: string;
+	options: string[];
+	selected: object | string | boolean | undefined;
+	priority?: number;
+};
+
 function App() {
-	const [count, setCount] = useState(0);
+	const [num, setNum] = useState(0);
+	const [form, setForm] = useState<Form[]>([
+		{
+			question: 'How are you feeling today?',
+			options: ['Happy', 'Neutral', 'Bad'],
+			selected: undefined,
+		},
+		{
+			question: 'Which do you prefer?',
+			options: ['Fiction', 'Non-fiction', "I don't mind"],
+			selected: undefined,
+		},
+		{
+			question: 'Favorite genres?',
+			options: ['Romance', 'SF', 'Fantasy', 'Horror', 'Mystery'],
+			selected: undefined,
+		},
+	]);
+	const [isLoading, setIsLoading] = useState(false);
+
+	const generateRecommendation = async () => {
+		setIsLoading(true);
+
+		try {
+			console.log('generating..');
+			await setTimeout(() => {
+				setIsLoading(false);
+			}, 2000);
+		} catch (err) {
+			console.error(err);
+		} /* finally {
+			setIsLoading(false);
+		} */
+	};
+
+	const handleResponse = async (questionNum: number, response: String) => {
+		console.log(questionNum, response);
+
+		// @todo num 값이 끝에 달했는지 확인
+		// if (questionNum >= form.length) {
+		// 	setIsLoading(true);
+
+		// 	await generateRecommendation();
+
+		// 	setIsLoading(false);
+		// 	return;
+		// }
+
+		const newForm = [...form];
+		newForm[questionNum] = {
+			...newForm[questionNum],
+			selected: response,
+		};
+		setForm(newForm);
+		setNum(num + 1);
+	};
+
+	useEffect(() => {
+		if (num == form.length) {
+			generateRecommendation();
+		}
+	}, [num]);
 
 	return (
 		<>
+			<p>Welcome! 🤗</p>
+
 			<div>
-				<a href="https://vite.dev" target="_blank">
-					<img src={viteLogo} className="logo" alt="Vite logo" />
-				</a>
-				<a href="https://react.dev" target="_blank">
-					<img src={reactLogo} className="logo react" alt="React logo" />
-				</a>
+				{isLoading ? (
+					<div> Generating recommendations ... </div>
+				) : (
+					<div>
+						{num < form.length ? (
+							<>
+								<div>{form[num].question}</div>
+								{form[num].options.map((op, idx) => (
+									<div key={idx} onClick={() => handleResponse(num, op)}>
+										{op}
+									</div>
+								))}
+							</>
+						) : (
+							<div> Recommendations : </div>
+						)}
+					</div>
+				)}
 			</div>
-			<h1>Vite + React</h1>
-			<div className="card">
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
-				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className="read-the-docs">
-				Click on the Vite and React logos to learn more
-			</p>
 		</>
 	);
 }
