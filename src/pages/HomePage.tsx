e804b061bf4ff2c88f9f41be4b50304d2ed9014f
Jiho Bok <bok.jiho@gmail.com>
2025-08-22 15:14:40 +0100

    feat: load bookshelf component using local storage data

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,8 +1,42 @@
+import { useEffect, useState } from 'react';
+import Bookshelf from '../components/Bookshelf';
+import { type BookshelfItem } from '../types';
+import { DEFAULT_BOOKSHELF_ITEM, DEFAULT_BOOKSHELF_KEY } from '../consts/books';
+
 export default function HomePage() {
+	const [bookshelfList, setBookshelfList] = useState<BookshelfItem[]>([]);
+
+	useEffect(() => {
+		// initialize books
+
+		const data = localStorage.getItem(DEFAULT_BOOKSHELF_KEY);
+		if (data) {
+			// setBooks(Object.values(JSON.parse(data)));
+			const parsed = JSON.parse(data);
+			console.log(parsed, '<<<< from LOCAL STORAGE');
+
+			// setBookshelfList(parsed);
+		} else {
+			const newBookshelf = {
+				...DEFAULT_BOOKSHELF_ITEM,
+				createdAt: Date.now(),
+				updatedAt: Date.now(),
+			};
+
+			setBookshelfList([newBookshelf]);
+		}
+	}, []);
+
 	return (
 		<>
-			<div>search bar</div>
-			<div>bookshelves</div>
+			<div>side bar</div>
+			{bookshelfList.length > 0 ? (
+				bookshelfList.map((b) => (
+					<Bookshelf key={b.key} name={b.name} books={b.books} />
+				))
+			) : (
+				<div>Empty</div>
+			)}
 		</>
 	);
 }
