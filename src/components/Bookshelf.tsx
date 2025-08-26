42593813623138a0a7e761f65f87860c483fc3be
Jiho Bok <bok.jiho@gmail.com>
2025-08-26 18:51:49 +0100

    refactor: replace with bookcard component

--- src/components/Bookshelf.tsx
+++ src/components/Bookshelf.tsx
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import type { Book, BookItem, BookshelfItem } from '../types';
-import useOpenLibraryAPI from '../hooks/useOpenLibraryAPI';
+import BookCard from './BookList/BookCard';
 
 // Bookshelf detail page
 export default function Bookshelf({
@@ -10,32 +10,24 @@ export default function Bookshelf({
 	name: string;
 	books: BookItem[];
 }) {
-	const { getBookCoverImage } = useOpenLibraryAPI();
 	useEffect(() => {
 		console.log(books, '<<<');
 	}, [books]);
 
 	return (
-		<div className="flex flex-col w-full h-full ">
-			<div>{name}</div>
+		<div className="flex flex-col overflow-y-scroll">
+			<h3 className="text-xl">{name}</h3>
 			<div>{books.length} books</div>
 			{/* <button type="button">View bookshelf</button> */}
-			<section className="flex gap-6 flex-wrap">
-				{books.length > 0 &&
-					books.map(({ book: b }: { book: Book }) => (
-						<div className="flex flex-col max-w-40 " key={b.key}>
-							<div className="w-full h-48">
-								<img
-									className="w-full h-full object-cover"
-									src={getBookCoverImage(b.coverEditionKey)}
-								/>
-							</div>
-							<div>Title: {b.title}</div>
-							<div>Author: {b.author}</div>
-							<div>Published: {b.publishedYear}</div>
-						</div>
+			{books.length > 0 && (
+				<section
+					className={`grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 p-6 max-w-6xl mx-auto `}
+				>
+					{books.map(({ book: b }: { book: Book }) => (
+						<BookCard book={b} onClickHandler={() => {}} />
 					))}
-			</section>
+				</section>
+			)}
 		</div>
 	);
 }
