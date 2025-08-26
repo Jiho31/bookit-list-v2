af61613454f29d5a22c440099eca479bb912d6bf
Jiho Bok <bok.jiho@gmail.com>
2025-08-26 15:54:41 +0100

    feat: use children props to render modal component

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -1,18 +1,27 @@
-function Modal({ isOpen, onClose }: { isOpen: boolean; onClose: () => void }) {
+function Modal({
+	children,
+	isOpen,
+	onClose,
+}: {
+	children: React.ReactNode;
+	isOpen: boolean;
+	onClose: () => void;
+}) {
 	return (
 		<>
 			{isOpen && (
-				<div
-					className="w-screen h-screen absolute top-0 left-0 z-999 bg-black bg-opacity-50 flex justify-center items-center"
-					onClick={onClose}
-				>
+				<>
 					<div
-						className="w-0.5 h-0.5 block bg-white rounded-2xl p-20"
+						className="w-screen h-screen absolute top-0 left-0 z-999 bg-black opacity-50 flex justify-center items-center"
+						onClick={onClose}
+					></div>
+					<div
+						className="w-1/2 min-w-1/2 h-1/2 absolute z-1000 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 block bg-white rounded-2xl p-20"
 						onClick={(e) => e.stopPropagation()}
 					>
-						Modal !!!
+						{children}
 					</div>
-				</div>
+				</>
 			)}
 		</>
 	);
