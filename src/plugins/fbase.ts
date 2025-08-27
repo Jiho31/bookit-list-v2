62e6b587f5f8e13146fbe702bb39cc3d5f831745
Jiho Bok <bok.jiho@gmail.com>
2025-08-27 17:58:08 +0100

    chore: implement firebase db settings

--- src/plugins/fbase.ts
+++ src/plugins/fbase.ts
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
 	apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
@@ -15,5 +16,6 @@ const firebaseConfig = {
 // Initialize Firebasea
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
+const db = getFirestore(app);
 
-export { auth as firebaseAuth };
+export { auth as firebaseAuth, db as firebaseDB };
