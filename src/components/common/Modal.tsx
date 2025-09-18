fc3bfcc62bba0bd0d1e23db66577d1c7295b8bb9
Jiho Bok <bok.jiho@gmail.com>
2025-09-18 12:40:04 +0100

    fix: center modal component and block scroll

--- src/components/common/Modal.tsx
+++ src/components/common/Modal.tsx
@@ -12,11 +12,11 @@ function Modal({
 			{isOpen && (
 				<>
 					<div
-						className="w-screen h-screen absolute top-0 left-0 z-999 bg-black opacity-50 flex justify-center items-center"
+						className="no-doc-scroll w-screen h-screen fixed top-0 left-0 z-999 bg-black opacity-50 flex justify-center items-center"
 						onClick={onClose}
 					></div>
 					<div
-						className="w-1/2 min-w-1/2 h-1/2 absolute z-1000 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 block bg-white rounded-2xl p-20"
+						className="w-1/2 min-w-1/2 h-1/2 fixed z-1000 top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 block bg-white rounded-2xl p-20"
 						onClick={(e) => e.stopPropagation()}
 					>
 						{children}
