b6253f7291e012c00b31357edc1d7435749b0899
Jiho Bok <bok.jiho@gmail.com>
2025-09-22 15:30:49 +0100

    feat: update signup page ui and routings

--- src/components/common/Layout.tsx
+++ src/components/common/Layout.tsx
@@ -53,12 +53,12 @@ export default function Layout() {
 									width="20px"
 								>
 									<path d="M200-120q-33 0-56.5-23.5T120-200v-560q0-33 23.5-56.5T200-840h280v80H200v560h280v80H200Zm440-160-55-58 102-102H360v-80h327L585-622l55-58 200 200-200 200Z" />
-									<title id="logout-button">Logout</title>
+									<title id="logout-button">Sign out</title>
 								</svg>
-								Logout
+								Sign out
 							</span>
 						) : (
-							<span>Login → </span>
+							<span>Sign in → </span>
 						)}
 					</button>
 				</div>
--- src/pages/AuthPage.tsx
+++ src/pages/AuthPage.tsx
@@ -56,7 +56,7 @@ const SignupForm = () => {
 		<form className="flex flex-col gap-3" onSubmit={handleSignup}>
 			<div className="flex gap-3">
 				<label
-					className="content-center basis-1/3 text-sm text-slate-400"
+					className="content-center basis-1/3 text-sm text-slate-600"
 					htmlFor="email"
 				>
 					Email
@@ -72,7 +72,7 @@ const SignupForm = () => {
 			</div>
 			<div className="flex gap-3">
 				<label
-					className="content-center basis-1/3 text-sm text-slate-400"
+					className="content-center basis-1/3 text-sm text-slate-600"
 					htmlFor="password"
 				>
 					Password
@@ -88,7 +88,7 @@ const SignupForm = () => {
 			</div>
 			<div className="flex gap-3">
 				<label
-					className="content-center basis-1/3 text-sm text-slate-400"
+					className="content-center basis-1/3 text-sm text-slate-600"
 					htmlFor="confirmPassword"
 				>
 					Confirm Password
@@ -177,7 +177,7 @@ const LoginForm = () => {
 
 	return (
 		<form className="flex flex-col gap-3" onSubmit={handleLogin}>
-			<label htmlFor="email" className="text-xs text-slate-400 mb-[-5px]">
+			<label htmlFor="email" className="text-xs text-slate-600 mb-[-5px]">
 				Email
 			</label>
 			<input
@@ -189,7 +189,7 @@ const LoginForm = () => {
 				value={email}
 				onChange={handleInputChange}
 			/>
-			<label htmlFor="password" className="text-xs text-slate-400 mb-[-5px]">
+			<label htmlFor="password" className="text-xs text-slate-600 mb-[-5px]">
 				Password
 			</label>
 			<input
@@ -208,10 +208,15 @@ const LoginForm = () => {
 				className="px-4 py-2.5 text-sm text-indigo-50 bg-indigo-600 hover:bg-indigo-800"
 				type="submit"
 			>
-				Login
+				Sign in
 			</button>
+			<div className="w-full flex items-center gap-2 text-sm text-slate-600">
+				<span className="h-[1px] bg-slate-300 w-full"></span>
+				or
+				<span className="h-[1px] bg-slate-300 w-full"></span>
+			</div>
 			<button
-				className="inline-flex gap-3 px-4 py-2 bg-white text-sm text-indigo-400 hover:text-indigo-50 border border-indigo-400 hover:bg-indigo-500"
+				className="inline-flex justify-center gap-3 px-4 py-2 bg-slate-100 text-sm text-slate-600 border border-slate-200 hover:bg-slate-200"
 				type="button"
 				onClick={() => onSocialLogin(OAUTH_PROVIDERS.GOOGLE)}
 			>
@@ -224,10 +229,10 @@ const LoginForm = () => {
 					<title>Google</title>
 					<path d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z" />
 				</svg>
-				Sign In with Google
+				Sign in with Google
 			</button>
 			<button
-				className="inline-flex gap-3 px-4 py-2 bg-white text-sm text-indigo-400 hover:text-indigo-50 border border-indigo-400 hover:bg-indigo-500"
+				className="inline-flex justify-center gap-3 px-4 py-2 bg-slate-100 text-sm text-slate-600 border border-slate-200 hover:bg-slate-200"
 				type="button"
 				onClick={() => onSocialLogin(OAUTH_PROVIDERS.GITHUB)}
 			>
@@ -240,35 +245,26 @@ const LoginForm = () => {
 					<title>GitHub</title>
 					<path d="M12 .297c-6.63 0-12 5.373-12 12 0 5.303 3.438 9.8 8.205 11.385.6.113.82-.258.82-.577 0-.285-.01-1.04-.015-2.04-3.338.724-4.042-1.61-4.042-1.61C4.422 18.07 3.633 17.7 3.633 17.7c-1.087-.744.084-.729.084-.729 1.205.084 1.838 1.236 1.838 1.236 1.07 1.835 2.809 1.305 3.495.998.108-.776.417-1.305.76-1.605-2.665-.3-5.466-1.332-5.466-5.93 0-1.31.465-2.38 1.235-3.22-.135-.303-.54-1.523.105-3.176 0 0 1.005-.322 3.3 1.23.96-.267 1.98-.399 3-.405 1.02.006 2.04.138 3 .405 2.28-1.552 3.285-1.23 3.285-1.23.645 1.653.24 2.873.12 3.176.765.84 1.23 1.91 1.23 3.22 0 4.61-2.805 5.625-5.475 5.92.42.36.81 1.096.81 2.22 0 1.606-.015 2.896-.015 3.286 0 .315.21.69.825.57C20.565 22.092 24 17.592 24 12.297c0-6.627-5.373-12-12-12" />
 				</svg>
-				Sign In with Github
+				Sign in with Github
 			</button>
+			{/* <div className="text-sm self-center">
+				New to Bookit List?
+				<button
+					onClick={() => toggleDisplay()}
+					className="ml-1 bg-inherit p-0 text-indigo-700 hover:underline"
+				>
+					Create an account
+				</button>
+			</div> */}
 		</form>
 	);
 };
 
 function UserAuthForm() {
-	const [display, setDisplay] = useState<'DEFAULT' | 'LOGIN' | 'SIGNUP'>(
-		'DEFAULT',
-	);
+	const [display, setDisplay] = useState<'LOGIN' | 'SIGNUP'>('LOGIN');
 
-	const BUTTONS = [
-		{
-			buttonName: 'Login',
-			clickHandler: () => setDisplay('LOGIN'),
-		},
-		{
-			buttonName: 'Sign Up',
-			clickHandler: () => setDisplay('SIGNUP'),
-		},
-	];
-
-	const displayContent = () => {
-		switch (display) {
-			case 'LOGIN':
-				return <LoginForm />;
-			case 'SIGNUP':
-				return <SignupForm />;
-		}
+	const toggleDisplay = () => {
+		setDisplay((prev) => (prev == 'LOGIN' ? 'SIGNUP' : 'LOGIN'));
 	};
 
 	return (
@@ -280,25 +276,34 @@ function UserAuthForm() {
 					alt="Bookit List logo"
 				/>
 				<h2 className="text-xl font-semibold mb-3">Welcome to Bookit List!</h2>
-				<p>Sign in or get registered to create your personal bookshelves!</p>
+				<p>Get started to create and manage your personal bookshelves!</p>
 			</div>
 
 			<div className="w-1/2 flex flex-col gap-3 justify-center">
-				{display === 'DEFAULT' ? (
-					<>
-						{BUTTONS.map((b, idx) => (
+				<>{display == 'LOGIN' ? <LoginForm /> : <SignupForm />}</>
+				<div className="text-sm self-center">
+					{display == 'LOGIN' ? (
+						<>
+							New to Bookit List?
 							<button
-								key={idx}
-								className="text-indigo-50 rounded-2xl px-4 py-3 bg-indigo-600 hover:bg-indigo-800"
-								onClick={b.clickHandler}
+								onClick={toggleDisplay}
+								className="ml-1 bg-inherit p-0 text-indigo-700 hover:underline"
 							>
-								{b.buttonName}
+								Create an account
 							</button>
-						))}
-					</>
-				) : (
-					<>{displayContent()}</>
-				)}
+						</>
+					) : (
+						<>
+							Already have an account?
+							<button
+								onClick={toggleDisplay}
+								className="ml-1 bg-inherit p-0 text-indigo-700 hover:underline"
+							>
+								Sign in
+							</button>
+						</>
+					)}
+				</div>
 			</div>
 		</section>
 	);
