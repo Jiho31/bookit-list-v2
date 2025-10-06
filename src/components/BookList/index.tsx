d4206de020edf809087c64391486c3d498890e2f
Jiho Bok <bok.jiho@gmail.com>
2025-10-06 21:08:51 +0100

    refactor: separate bookshelf modal component

--- src/components/BookList/index.tsx
+++ src/components/BookList/index.tsx
@@ -1,74 +1,11 @@
-import { useEffect, useMemo, useState, type ReactElement } from 'react';
-import { toast } from 'sonner';
+import { useEffect, useMemo, type ReactElement } from 'react';
 import type { Book, CardButton } from '../../types';
 import BookCard from './BookCard';
-import { useBookshelf } from '../../contexts/BookshelfContext';
 import { useModal } from '../../contexts/ModalContext';
 import { useAuth } from '@/contexts/AuthContext';
 import RegisterModal from '../RegisterModal';
 import LoadingSpinner from '../common/LoadingSpinner';
-
-function BookshelfListModal({
-	book,
-	close,
-}: {
-	book: Book;
-	close: () => void;
-}) {
-	const { list, addBookToShelf } = useBookshelf();
-	const [targetKey, setTargetKey] = useState('');
-
-	const addToBookshelf = async () => {
-		if (targetKey === '') {
-			toast.info('You have to select one bookshelf');
-			return;
-		}
-
-		try {
-			await addBookToShelf(targetKey, book);
-			toast.success('Successfully added book to bookshelf');
-		} catch (err) {
-			if (typeof err === 'string') {
-				toast.info(err);
-			} else {
-				console.error(err);
-			}
-		} finally {
-			close();
-		}
-	};
-
-	return (
-		<div className="flex flex-col gap-2 w-[350px] h-auto max-h-2/3 overflow-scroll bg-slate-200 p-5 rounded-2xl">
-			<h2 className="text-md font-semibold py-1 mb-5">Add to bookshelf</h2>
-			<ul className="w-full flex flex-col gap-1.5">
-				{list.map((item) => (
-					<li
-						className={`rounded-lg px-3 py-2 text-sm ${item.key === targetKey ? 'bg-indigo-400 text-indigo-50' : 'bg-white'}  hover:bg-indigo-600 hover:text-indigo-50 cursor-pointer`}
-						key={item.key}
-						onClick={() => setTargetKey(item.key)}
-					>
-						{item.name} ({item.numOfBooks})
-					</li>
-				))}
-			</ul>
-			<button
-				type="button"
-				className="mt-2 w-min self-center"
-				onClick={addToBookshelf}
-			>
-				Add
-			</button>
-			<button
-				type="button"
-				className="text-slate-800 bg-slate-200 absolute top-5 right-5 p-2"
-				onClick={close}
-			>
-				X
-			</button>
-		</div>
-	);
-}
+import BookshelfListModal from './BookshelfListModal';
 
 function BookList({
 	data,
